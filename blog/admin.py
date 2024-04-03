92e3fdfa0e1d5d51de545d3c52250a9bd5a18187
Nguyen Minh Duc <ducnm2@vmogroup.com>
2024-04-03 11:36:00 +0700

    test

---
+++ blog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Post, Tag, Author
+
+# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('author', 'tags', 'date')
+    list_display = ('title', 'date', 'author')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
+admin.site.register(Author)
